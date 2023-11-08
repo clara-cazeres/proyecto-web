9cbf0ef8259142e33f535da7c2e53c6c26629689
clara-cazeres <claracazeres@gmail.com>
2023-11-08 20:24:12 -0300

    ventana modal

--- scripts/shows.js
+++ scripts/shows.js
@@ -1,5 +1,6 @@
 const tablaShows = document.querySelector("#tabla-shows");
 
+//funciones generales
 function limpiarTabla() {
     tablaShows.innerHTML = "";
     tablaShows.innerHTML = `
@@ -14,54 +15,84 @@ function limpiarTabla() {
 }
 
 function agregarFilaShow(show) {
+    const fecha = new Date(show.fecha);
+    const opcionesFecha = { weekday: 'long', day: 'numeric', month: 'long' };
+    const fechaFormateada = fecha.toLocaleDateString('es-ES', opcionesFecha);
+
     const fila = document.createElement("tr");
     fila.innerHTML += `
-    <td>${show.fecha}</td>
+    <td>${fechaFormateada}</td>
     <td>${show.lugar} - ${show.ciudad}, ${show.pais}</td>
     <td><a class="bt-tabla" href="${show.entradas}">
-    <p>comprar</p>
-    <span class="material-symbols-outlined">
-        local_activity
-    </span>
+        <p>comprar</p>
+        <span class="material-symbols-outlined">
+            local_activity
+        </span>
     </a>
     </td>
     <td>
-    <a class="bt-tabla" href="">
-        <p>ingresar</p>
+        <div class="bt-tabla link-kuartito"> 
+            <p>ingresar</p>
             <span class="material-symbols-outlined">arrow_forward</span>
-    </a>
+        </div>
     </td>
-`;
+    `;
     tablaShows.appendChild(fila);
 }
 
-function listarShows() {
-    fetch('http://localhost:3001/shows')
+function agregarEventListenersADinamicos() {
+    const botonesAbrirKuartito = document.querySelectorAll(".link-kuartito");
+    const botonesCerrarKuartito = document.querySelectorAll(".cerrar-modal")
+    botonesAbrirKuartito.forEach(boton => {
+        boton.addEventListener('click', abrirVentanaModal);
+    });
+    botonesCerrarKuartito.forEach(boton => {
+        boton.addEventListener('click', cerrarVentanaModal);
+    });
+}
+
+
+//listar todos los elementos
+
+function listarShows(tipo = '') {
+    let url = 'http://localhost:3001/shows';
+    if (tipo !== '') {
+        url += `?tipo=${tipo}`;
+    }
+
+    fetch(url)
         .then(response => response.json())
         .then(data => {
-            limpiarTabla(); //limpio la tabla
-            //itero sobre los registros devueltos por el backend
+            limpiarTabla();
             data.forEach(show => {
                 agregarFilaShow(show);
             });
+            agregarEventListenersADinamicos();
         })
         .catch(error => console.error('Error:', error));
 }
+
 
 listarShows();
-/* function listarShows() {
-    fetch('http://localhost:3001/shows')
-        .then(response => response.json())
-        .then(shows => {
-            const showsContainer = document.getElementById('prueba-listar');
-            shows.forEach(show => {
-                const div = document.createElement('div');
-                div.innerHTML = `Ciudad: ${show.ciudad} - Fecha: ${show.fecha}`;
-                showsContainer.appendChild(div);
-            });
-        })
-        .catch(error => console.error('Error:', error));
+
+//filtros por fecha
+
+document.getElementById('fechas-pasadas').addEventListener('change', () => listarShows('pasadas'));
+
+document.getElementById('fechas-proximas').addEventListener('change', () => listarShows('proximas'));
+
+document.getElementById('todas-fechas').addEventListener('change', () => listarShows());
+
+
+//kuartito ventana modal
+
+let ventanaModal = document.querySelector(".fondo-modal");
+
+function abrirVentanaModal() {
+  ventanaModal.classList.remove("ocultar");
+  ventanaModal.classList.add("mostrar-modal");
 }
-listarShows(); */
-
-
+function cerrarVentanaModal() {
+    ventanaModal.classList.remove("mostrar-modal")
+    ventanaModal.classList.add("ocultar");
+}
