2210dbd41a301ce88521076b6e224e1d95b1a466
clara-cazeres <claracazeres@gmail.com>
2023-12-14 17:23:52 -0300

    comentario y fotos funcoinando

--- scripts/kuartito/kuartito.js
+++ scripts/kuartito/kuartito.js
@@ -1,51 +1,59 @@
 console.log("Script cargado");
 
-// Función para cargar la información del show
+// funcion mostrar info del show 
 function cargarInfoShow(showId) {
     console.log("Llamando a cargarInfoShow con showId:", showId);
     fetch(`http://localhost:3001/shows/${showId}`)
         .then(response => response.json())
         .then(show => {
-            document.getElementById('show-fecha').textContent += formatearFecha(show.fecha);
-            document.getElementById('show-lugar').textContent += show.lugar;
-            document.getElementById('show-ciudad-pais').textContent += `${show.ciudad}, ${show.pais}`;
-            document.getElementById('show-asistentes').textContent += show.asistentes;
+            document.getElementById('show-fecha').textContent = formatearFecha(show.fecha);
+            document.getElementById('show-lugar').textContent = show.lugar;
+            document.getElementById('show-ciudad-pais').textContent = `${show.ciudad}, ${show.pais}`;
+            document.getElementById('show-integrantes').textContent = '100';  // Ajusta según sea necesario
             console.log("Datos del show cargados:", show);
-            mostrarImagenes(show.images);})
+            mostrarImagenes(show.images);
+        })
         .catch(error => console.error('Error en cargarInfoShow:', error));
 }
 
+//obtener el id de la url 
 function obtenerShowIdDeURL() {
     const parametrosURL = new URLSearchParams(window.location.search);
     return parametrosURL.get('showId');
 }
-
+//formatear fecha para mostrarla 
 function formatearFecha(fecha) {
     const opciones = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     return new Date(fecha).toLocaleDateString('es-ES', opciones);
 }
 
-// SUBIR IMAGENES
-document.addEventListener("DOMContentLoaded", (event) => {
+//mostrar las imagenes subidas
+function mostrarImagenes(images) {
+    const photosContainer = document.getElementById("photos");
+    images.forEach(imageUrl => {
+        const img = document.createElement("img");
+        img.src = imageUrl;
+        photosContainer.appendChild(img);
+    });
+}
+
+//muestra la info del show y contiene el fetch para subir las fotos
+document.addEventListener("DOMContentLoaded", () => {
     console.log("DOMContentLoaded");
 
     const showId = obtenerShowIdDeURL();
     if (showId) {
         cargarInfoShow(showId);
+        cargarComentarios(showId);
     } else {
         console.error("No se encontró showId en la URL.");
+        return;
     }
+   
 
     const uploadButton = document.getElementById("upload");
-    if (!uploadButton) {
-        console.error("El botón de subida no se encontró.");
-        return;
-    }
     const photos = document.getElementById("photos");
-    if (!photos) {
-        console.error("El contenedor de fotos no se encontró.");
-        return;
-    }
+
 
     uploadButton.addEventListener("click", (e) => {
         console.log("Click en el botón de subida.");
@@ -57,6 +65,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
         }
         const formData = new FormData();
         formData.append("image", files[0]);
+        formData.append("showId", showId);
+        console.log("ShowId a enviar:", showId);
 
         fetch("http://localhost:3001/uploads-show-pic", {
             method: "POST",
@@ -64,7 +74,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
         })
             .then((response) => {
                 if (!response.ok) {
-                    throw new Error("hubo un problema con la consulta");
+                    throw new Error("Hubo un problema con la consulta");
                 }
                 return response.json();
             })
@@ -81,24 +91,41 @@ document.addEventListener("DOMContentLoaded", (event) => {
             .catch(error => {
                 console.error("Hubo un problema subiendo la foto:", error);
             });
-            
     });
 });
+
+//mostrar comentarios subidos
+function cargarComentarios(showId) {
+    fetch(`http://localhost:3001/comentarios/${showId}`)
+        .then(response => response.json())
+        .then(comentarios => {
+            const divComentarios = document.querySelector('#div-comentarios');
+            divComentarios.innerHTML = ''; 
+            
+            comentarios.forEach(comentario => {
+                const pComentario = document.createElement('p');
+                pComentario.textContent = comentario.texto;
+                divComentarios.appendChild(pComentario);
+            });
+        })
+        .catch(error => console.error('Error al cargar comentarios:', error));
+}
+
 
 
 //comentarios
 document.getElementById('enviar-comentario').addEventListener('click', function () {
     const comentarioTexto = document.getElementById('texto-comentario').value;
-    const customShowId = `${customId}`; // Asegúrate de reemplazar esto con el ID real del show
+    const showId = obtenerShowIdDeURL();
 
-    fetch('/comentarios', {
+    fetch("http://localhost:3001/comentarios", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
             comentario: comentarioTexto,
-            showId: customShowId
+            showId: showId,
         })
     })
         .then(response => {
@@ -117,9 +144,12 @@ document.getElementById('enviar-comentario').addEventListener('click', function 
             document.getElementById('texto-comentario').value = ''; // Limpia el textarea
 
             // Actualiza la UI
-            const divComentarios = document.createElement('div');
-            divComentarios.textContent = comentarioTexto;
-            document.querySelector('.interaccion-usuario').appendChild(divComentarios);
+            const divComentarios = document.querySelector('#div-comentarios');
+            const pComentario = document.createElement('p'); 
+            pComentario.textContent = comentarioTexto; 
+            divComentarios.appendChild(pComentario); 
+            
+
         })
         .catch(error => {
             console.error('Error:', error);
